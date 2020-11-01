a322af5117978770140e0d0b8b4f2daf3b3dea89
Sergey Petrelevich <petrelevich@yandex.ru>
2020-11-01 23:52:58 +0300

    L10 byteCode

---
+++ L10-byteCodes/src/main/java/ru/otus/aop/instrumentation/setter/SetterDemo.java
@@ -0,0 +1,19 @@
+package ru.otus.aop.instrumentation.setter;
+
+/*
+    java -javaagent:setterDemo.jar -jar setterDemo.jar
+*/
+public class SetterDemo {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("main");
+    MyClass demo = new MyClass();
+    System.out.println(demo.getValue());
+    modifyPrivateValue(demo);
+    System.out.println(demo.getValue());
+  }
+
+  private static void modifyPrivateValue(MyClass demo) throws Exception {
+    demo.getClass().getMethod("setValue", int.class).invoke(demo, -4);
+  }
+}
--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,4 +2,5 @@ public interface Versions {
     String guava = "28.2-jre";
     String jmh = "1.26";
     String jol = "0.14";
+    String asm = "9.0";
 }
