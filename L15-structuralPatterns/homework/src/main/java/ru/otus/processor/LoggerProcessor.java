fa883cca71a0bed9d98a4cc0dab4cad87daa5743
Sergey Petrelevich <petrelevich@yandex.ru>
2020-12-06 18:49:18 +0300

    L15 updated todo

--- L15-structuralPatterns/homework/src/main/java/ru/otus/processor/LoggerProcessor.java
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/processor/LoggerProcessor.java
@@ -4,6 +4,7 @@ import ru.otus.model.Message;
 
 public class LoggerProcessor implements Processor {
     //todo: 3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
+    //         Секунда должна определяьться во время выполнения.
 
     private final Processor processor;
 
