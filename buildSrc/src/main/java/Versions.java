2e8371dcf3e855ce27159a99caa656203381580e
Александр Оруджев <stvort@mail.ru>
2021-01-10 16:52:12 +0400

    L23-webServer added and updated

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -18,4 +18,11 @@ public interface Versions {
     String cassandra = "4.9.0";
     String neo4j = "4.2.0";
     String jedis = "3.4.0";
+
+    String jetty = "11.0.0";
+//    String jetty = "9.4.35.v20201120";
+//    String jetty = "9.4.31.v20200723";
+    String freemarker = "2.3.30";
+    String reflections = "0.9.12";
+
 }
