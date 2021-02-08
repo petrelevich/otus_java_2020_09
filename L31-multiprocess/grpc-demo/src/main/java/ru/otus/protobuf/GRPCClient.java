ef6f055c4fb4a055f7b120da2f4a15a811ecc53e
Александр Оруджев <stvort@mail.ru>
2021-02-08 11:51:56 +0400

    L31-multiprocess гзвфеув

--- L31-multiprocess/grpc-demo/src/main/java/ru/otus/protobuf/GRPCClient.java
+++ L31-multiprocess/grpc-demo/src/main/java/ru/otus/protobuf/GRPCClient.java
@@ -2,18 +2,20 @@ package ru.otus.protobuf;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import ru.otus.protobuf.generated.Empty;
 import ru.otus.protobuf.generated.RemoteDBServiceGrpc;
 import ru.otus.protobuf.generated.UserMessage;
 
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
 
 public class GRPCClient {
 
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 8090;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
                 .usePlaintext()
                 .build();
@@ -32,6 +34,30 @@ public class GRPCClient {
                 System.out.println(String.format("{id: %d, name: %s %s}",
                         um.getId(), um.getFirstName(), um.getLastName()))
         );
+
+        CountDownLatch latch = new CountDownLatch(1);
+        System.out.println("\n\n\nА теперь тоже самое, только асинхронно!!!\n\n");
+        RemoteDBServiceGrpc.RemoteDBServiceStub newStub = RemoteDBServiceGrpc.newStub(channel);
+        newStub.findAllUsers(Empty.getDefaultInstance(), new StreamObserver<UserMessage>() {
+            @Override
+            public void onNext(UserMessage um) {
+                System.out.println(String.format("{id: %d, name: %s %s}",
+                        um.getId(), um.getFirstName(), um.getLastName()));
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.err.println(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("\n\nЯ все!");
+                latch.countDown();
+            }
+        });
+
+        latch.await();
 
         channel.shutdown();
     }
